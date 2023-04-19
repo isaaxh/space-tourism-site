69bc116673eb2ff0ba385f37c61a2abe1a6ed88e
Isaac Hussain <isaaclearns58@gmail.com>
2023-04-19 11:34:15 +0300

    error: still cannot use svg, now looking for vite svgr plugin solution

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,8 @@ import { ReactComponent as Logo } from '../assets/shared/logo.svg';
 const Header = () => {
   return (
     <div>
-        <img src="../assets/shared/logo.svg" alt="space-tourism-logo" />
+        <h1>Header</h1>
+        <Logo />
     </div>
   )
 }
