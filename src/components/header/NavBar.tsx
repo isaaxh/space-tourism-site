4d167f72fd6220ed00751e050466436224f8ea58
Isaac Hussain <isaaclearns58@gmail.com>
2023-04-23 20:26:46 +0300

    home design for the tablet

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,11 +1,11 @@
 
 const NavBar = () => {
   return (
-    <ul className='nav-bar'>
-        <li className='nav-items'><span className='nav-number'>00</span> Home</li>
-        <li className='nav-items'><span className='nav-number'>01</span> Destination</li>
-        <li className='nav-items'><span className='nav-number'>02</span> Crew</li>
-        <li className='nav-items'><span className='nav-number'>03</span> Technology</li>
+    <ul className='nav-bar glass'>
+        <li className='nav-links'><span className='nav-number'>00</span> Home</li>
+        <li className='nav-links'><span className='nav-number'>01</span> Destination</li>
+        <li className='nav-links'><span className='nav-number'>02</span> Crew</li>
+        <li className='nav-links'><span className='nav-number'>03</span> Technology</li>
     </ul>
   )
 }
