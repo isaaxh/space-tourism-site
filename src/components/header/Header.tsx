a6079d3f6e8e57d2463253315565a7a16da68e16
Isaac Hussain <isaaclearns58@gmail.com>
2023-04-23 18:25:11 +0300

    change file paths for header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,30 @@
+import { ReactComponent as Logo } from '../../assets/shared/logo.svg';
+import { ReactComponent as BurgerMenu } from '../../assets/shared/icon-hamburger.svg';
+import { useEffect, useState } from 'react';
+import NavBar from './NavBar';
+
+const Header = () => {
+  const [isDesktop, setDesktop] = useState(window.innerWidth < 1024);
+  const [isTablet, setTablet] = useState(window.innerWidth < 768);
+  const [isMobile, setMobile] = useState(window.innerWidth < 480);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth < 1024);
+    setTablet(window.innerWidth < 768);
+    setMobile(window.innerWidth < 480);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
+  return (
+    <div className='header'>
+        <Logo />
+        {!isMobile ? <NavBar /> : <BurgerMenu />}
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/header/NavBar.tsx
@@ -0,0 +1,13 @@
+
+const NavBar = () => {
+  return (
+    <ul className='nav-bar'>
+        <li className='nav-items'><span className='nav-number'>00</span> Home</li>
+        <li className='nav-items'><span className='nav-number'>01</span> Destination</li>
+        <li className='nav-items'><span className='nav-number'>02</span> Crew</li>
+        <li className='nav-items'><span className='nav-number'>03</span> Technology</li>
+    </ul>
+  )
+}
+
+export default NavBar
