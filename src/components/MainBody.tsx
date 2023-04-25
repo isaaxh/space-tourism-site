59d387141dc45dba7ab6a284385f458d60a3e917
Isaac Hussain <isaaclearns58@gmail.com>
2023-04-25 09:07:27 +0300

    add css module for home page

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
+import Home from '../pages/home/Home';
 
 const MainBody = () => {
   return (
-    <div className='main-body'>
-      <div className="intro-container">
-        <h5 className='sub-heading'>SO, YOU WANT TO TRAVEL TO</h5>
-        <h1 className='heading'>SPACE</h1>
-        <p className='description'>
-          Let’s face it; if you want to go to space, you might as well genuinely go to 
-          outer space and not hover kind of on the edge of it. Well sit back, and relax 
-          because we’ll give you a truly out of this world experience!
-        </p>
-      </div>
-      <div className="btn-container">
-        <button className='btn-main'>EXPLORE</button>
-      </div>
-    </div>
+    <>
+    <Home/>
+    </>
   )
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Style from './Home.module.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={Style['main-body']}>
+      <div className={Style["intro-container"]}>
+        <h5 className={Style['sub-heading']}>SO, YOU WANT TO TRAVEL TO</h5>
+        <h1 className={Style.heading}>SPACE</h1>
+        <p className={Style.description}>
+          Let’s face it; if you want to go to space, you might as well genuinely go to 
+          outer space and not hover kind of on the edge of it. Well sit back, and relax 
+          because we’ll give you a truly out of this world experience!
+        </p>
+      </div>
+      <div className={Style["btn-container"]}>
+        <button className={Style['btn-main']}>EXPLORE</button>
+      </div>
+    </div>
   )
 }
 
