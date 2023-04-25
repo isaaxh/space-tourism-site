e94e780e5a05d5843d717979cd3496887a213644
Isaac Hussain <isaaclearns58@gmail.com>
2023-04-25 16:12:01 +0300

    update h1 page name on each page

--- src/pages/crew/Crew.tsx
+++ src/pages/crew/Crew.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 const Crew = () => {
   return (
-    <div>Crew</div>
+    <h1>Crew</h1>
   )
 }
 
--- src/pages/technology/Technology.tsx
+++ src/pages/technology/Technology.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Technology = () => {
   return (
-    <div>Technology</div>
+    <h1>Technology</h1>
   )
 }
 
