9513f38d3fbed42a24469da16e6e5a94e98e5fae
Isaac Hussain <isaaclearns58@gmail.com>
2023-04-25 09:11:28 +0300

    add all the pages to the Main body to render them conditionally

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Home from '../pages/home/Home';
+import Destination from '../pages/destination/Destination';
+import Crew from '../pages/crew/Crew';
+import Technology from '../pages/technology/Technology';
 
 const MainBody = () => {
   return (
     <>
     <Home/>
+    {/* <Destination/> */}
+    {/* <Crew/> */}
+    {/* <Technology/> */}
     </>
   )
 }
