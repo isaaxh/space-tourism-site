98fefbab5373e55ac5d2fd53fe1a6382d713e51a
Isaac Hussain <isaaclearns58@gmail.com>
2023-05-09 10:33:24 +0300

    add swiper js layout for the slider

--- src/pages/crew/Crew.tsx
+++ src/pages/crew/Crew.tsx
@@ -1,7 +1,36 @@
+import Style from './Crew.module.css';
+import crew1 from '../../assets/crew/image-douglas-hurley.png'
 
 const Crew = () => {
+
   return (
-    <h1>Crew</h1>
+    <div className={Style.container}>
+      <h5 className={Style['sub-title']}><span className={Style['sub-title-number']}>02</span>Meet your crew</h5>
+      <div className="slider-container">
+      {/* <!-- Slider main container --> */}
+        <div className="swiper">
+          {/* <!-- Additional required wrapper --> */}
+          <div className="swiper-wrapper">
+            {/* <!-- Slides --> */}
+            <div className="swiper-slide">Slide 1</div>
+            <div className="swiper-slide">Slide 2</div>
+            <div className="swiper-slide">Slide 3</div>
+            ...
+          </div>
+          {/* <!-- If we need pagination --> */}
+          <div className="swiper-pagination"></div>
+
+          {/* <!-- If we need navigation buttons --> */}
+          <div className="swiper-button-prev"></div>
+          <div className="swiper-button-next"></div>
+
+          {/* <!-- If we need scrollbar --> */}
+          <div className="swiper-scrollbar"></div>
+        </div>
+      </div>
+      <hr />
+
+    </div>
   )
 }
 
