313100afb28f9da5be630c221f5a8426b6a789a3
Isaac Hussain <isaaclearns58@gmail.com>
2023-05-12 10:11:56 +0300

    add all four imgs to the swiperslid components

--- src/pages/crew/components/Slider.tsx
+++ src/pages/crew/components/Slider.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import imgDouglas from '../../../assets/crew/image-douglas-hurley.png';
-import imgVictor from '../../../assets/crew/image-victor-glover.png';
-import imgAnousheh from '../../../assets/crew/image-anousheh-ansari.png';
-import imgMark from '../../../assets/crew/image-mark-shuttleworth.png';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import 'swiper/swiper-bundle.css'; 
+import img1 from '../../../assets/crew/image-douglas-hurley.png';
+import img2 from '../../../assets/crew/image-victor-glover.png';
+import img3 from '../../../assets/crew/image-anousheh-ansari.png';
+import img4 from '../../../assets/crew/image-mark-shuttleworth.png';
 
 const Slider = () => {
+    const slides = [];
+    const imgs = [img1, img2, img3, img4];
 
+    for (let i = 0; i < 5; i += 1) {
+        slides.push(
+            <SwiperSlide key={`slide-${i}`}> 
+                <img 
+                    src={imgs[i]} 
+                    alt={`img-${i}`}
+                    />
+            </SwiperSlide>
+        );
+    }
    
   return (
     <>
-        
+        <Swiper id='main'>
+            {slides}
+        </Swiper>
     </>
   )
 }
